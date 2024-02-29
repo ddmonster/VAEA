c94c44f923d0e950fbd1af7df94c93bd9722d6ab
ddmosnter <ddmonster.wei@outloook.com>
2024-02-29 23:24:12 +0000

    demo ready

--- app.py
+++ app.py
@@ -1,57 +1,75 @@
 import fastapi
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import RedirectResponse
-from modelservice.main import Product_Introduce_NLG
 from result_sample import result as fake_result
-import shutil
 from pathlib import Path
-from pydantic  import BaseModel
+from pydantic import BaseModel
+import re
 file_path = Path(__file__).parent.resolve()
 static_dir = file_path.joinpath("frontend")
 chart_dir = file_path.joinpath("modelservice").joinpath("chart").joinpath("chart_html")
 img_dir = file_path.joinpath("modelservice").joinpath("database").joinpath("images")
+
+
 def test_in_app():
     pass
 
 
 app = fastapi.FastAPI()
-app.state.model = Product_Introduce_NLG()
+# app.state.model = Product_Introduce_NLG()
 app.mount("/static", StaticFiles(directory=static_dir.resolve()), name="static")
 app.mount("/chart", StaticFiles(directory=chart_dir.resolve()), name="chart")
 app.mount("/img", StaticFiles(directory=img_dir.resolve()), name="img")
+
+
 @app.get("/")
 async def index_page():
     return RedirectResponse("/static/index.html")
+
+
 def result_process(result: dict):
-    img_path = Path(result["image_path"])
-    result["image_path"] = f"/img/{img_path.name}"
+    get_name = lambda x: x.split("\\")[-1]
+    m = re.search("[0-1A-Za-z].*", result["product_introduction"])
+    result["product_introduction"]
+    img_path = get_name(result["image_path"])
+    result["image_path"] = f"/img/{img_path}"
     d1 = result.get("dashboard_1")
     d2 = result.get("dashboard_2")
     d3 = result.get("dashboard_3")
     if d1:
-        result["dashboard_1"] = f"/chart/{Path(d1['html_path']).name}"
+        name = get_name(d1["html_path"])
+        result["dashboard_1"]["html_path"] = f"/chart/{name}"
     if d2:
-        result["dashboard_2"] = f"/chart/{Path(d2['html_path']).name}"
+        name = get_name(d2["html_path"])
+        result["dashboard_2"]["html_path"] = f"/chart/{name}"
     if d3:
-        result["dashboard_3"] = f"/chart/{Path(d3['html_path']).name}"
+        name = get_name(d3["html_path"])
+        result["dashboard_3"]["html_path"] = f"/chart/{name}"
     return result
+
+
 class Instruction(BaseModel):
-        instruction: str
+    instruction: str
+
+
 @app.post("/instruction/")
-async def invoke_model(instruction:Instruction):
+async def invoke_model(instruction: Instruction):
     print(instruction)
     result: dict = app.state.model.main_run(sentence=instruction)
     print(result)
     return result_process(result)
 
 
-@app.get("/instruction_t/")
-async def invoke_model_test_data():
-    return result_process(fake_result)
+from copy import deepcopy
 
+
+@app.post("/instruction_t/")
+async def invoke_model_test_data(instruction: Instruction):
+    rest = deepcopy(fake_result)
+    return result_process(rest)
 
 
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app,host="127.0.0.1",port=8899)
+    uvicorn.run(app, host="127.0.0.1", port=8899)
