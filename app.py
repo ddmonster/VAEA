04f3e096f5b8de4fb3f8ceefefcd97c3d6fcc613
ddmosnter <ddmonster.wei@outloook.com>
2024-03-01 14:15:32 +0000

    filter  emoji

--- app.py
+++ app.py
@@ -10,6 +10,13 @@ file_path = Path(__file__).parent.resolve()
 static_dir = file_path.joinpath("frontend")
 chart_dir = file_path.joinpath("modelservice").joinpath("chart").joinpath("chart_html")
 img_dir = file_path.joinpath("modelservice").joinpath("database").joinpath("images")
+import re
+
+
+def filter_str(desstr, restr=""):
+    # 过滤除中英文及数字以外的其他字符
+    res = re.compile("[^\\u4e00-\\u9fa5^a-z^A-Z^0-9\.\,\n\ ]")
+    return res.sub(restr, desstr)
 
 
 def test_in_app():
@@ -29,9 +36,9 @@ async def index_page():
 
 
 def result_process(result: dict):
+    print(result)
     get_name = lambda x: x.split("\\")[-1]
-    m = re.search("[0-1A-Za-z].*", result["product_introduction"])
-    result["product_introduction"]
+    result["product_introduction"] = filter_str(result["product_introduction"])
     img_path = get_name(result["image_path"])
     result["image_path"] = f"/img/{img_path}"
     d1 = result.get("dashboard_1")
@@ -39,12 +46,15 @@ def result_process(result: dict):
     d3 = result.get("dashboard_3")
     if d1:
         name = get_name(d1["html_path"])
+        d1["text"] = filter_str(d1["text"])
         result["dashboard_1"]["html_path"] = f"/chart/{name}"
     if d2:
         name = get_name(d2["html_path"])
+        d2["text"] = filter_str(d2["text"])
         result["dashboard_2"]["html_path"] = f"/chart/{name}"
     if d3:
         name = get_name(d3["html_path"])
+        d3["text"] = filter_str(d3["text"])
         result["dashboard_3"]["html_path"] = f"/chart/{name}"
     return result
 
